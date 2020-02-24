5b3ae864d0039fcb86f7cdb51cac0c922a82847b
Clara <UO264958@uniovi.es>
2020-02-24 12:47:30 +0100

    SDI - SpringBoot - Sesión - 4.3 - Control de acceso por roles

--- sdi1920-211-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi1920-211-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,8 +1,5 @@
 package com.uniovi.controllers;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,7 +35,7 @@ public class MarksController {
 
 	@RequestMapping("/mark/list")
 	public String getList(Model model) {
-		
+
 		model.addAttribute("markList", marksService.getMarks());
 		// Tiene que devolver una vista
 		return "mark/list";
@@ -93,6 +90,18 @@ public class MarksController {
 	@RequestMapping("/mark/delete/{id}")
 	public String deleteMark(@PathVariable Long id) {
 		marksService.deleteMark(id);
+		return "redirect:/mark/list";
+	}
+
+	@RequestMapping(value = "/mark/{id}/resend", method = RequestMethod.GET)
+	public String setResendTrue(Model model, @PathVariable Long id) {
+		marksService.setMarkResend(true, id);
+		return "redirect:/mark/list";
+	}
+
+	@RequestMapping(value = "/mark/{id}/noresend", method = RequestMethod.GET)
+	public String setResendFalse(Model model, @PathVariable Long id) {
+		marksService.setMarkResend(false, id);
 		return "redirect:/mark/list";
 	}
 
