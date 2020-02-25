400e6e47ec6067b96a8462f8548a801d2a3f989d
Clara <UO264958@uniovi.es>
2020-02-25 23:45:19 +0100

    SDI - SpringBoot - Sesión - 4. 6 - Paginación

--- sdi1920-211-lab-spring/src/main/java/com/uniovi/repositories/MarksRepository.java
+++ sdi1920-211-lab-spring/src/main/java/com/uniovi/repositories/MarksRepository.java
@@ -1,14 +1,14 @@
 package com.uniovi.repositories;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.uniovi.entities.Mark;
-import com.uniovi.entities.User;
+import com.uniovi.entities.User;	
 
 public interface MarksRepository extends CrudRepository<Mark, Long> {
 	@Modifying
@@ -16,12 +16,15 @@ public interface MarksRepository extends CrudRepository<Mark, Long> {
 	@Query("UPDATE Mark SET resend = ?1 WHERE id = ?2")
 	void updateResend(Boolean resend, Long id);
 
+	Page<Mark> findAll(Pageable pageable);
+
 	@Query("SELECT r FROM Mark r WHERE r.user = ?1 ORDER BY r.id ASC ")
-	List<Mark> findAllByUser(User user);
+	Page<Mark> findAllByUser(Pageable pageable, User user);
 
 	@Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER (r.user.name) LIKE LOWER(?1))")
-	List<Mark> searchByDescriptionAndName(String searchtext);
+	Page<Mark> searchByDescriptionAndName(Pageable pageable, String searchtext);
 
 	@Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER (r.user.name) LIKE LOWER(?1)) AND r.user = ?2")
-	List<Mark> searchByDescriptionNameAndUser(String searchtext, User user);
+	Page<Mark> searchByDescriptionNameAndUser(Pageable pageable, String searchtext, User user);
+
 }
